cc9e5b76aa94bb269bdf505bf8ff34a683abee28
Andrei Lucaci <contact@andreilucaci.ro>
2018-10-23 01:55:31 +0300

    First commit - added algorithm

---
+++ CollatzConjecture/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CollatzConjecture.Engine;
+
+namespace CollatzConjecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Collatz Conjecture program");
+
+            var engine = new CollatzConjectureEngine();
+
+            while (true)
+            {
+                Console.Write("Input number: ");
+                var numberLn = Console.ReadLine();
+
+                if (numberLn == "q")
+                {
+                    break;
+                }
+                if (!ulong.TryParse(numberLn, out var number))
+                {
+                    Console.WriteLine($"Invalid number: {numberLn}");
+                }
+                else
+                {
+                    var steps = engine.Compute(number);
+
+                    Console.WriteLine($"Done in {steps} steps.");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
