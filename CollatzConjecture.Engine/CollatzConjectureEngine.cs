a483b49c991dcaae917d316af3060e6d3e8ca89a
Andrei Lucaci <contact@andreilucaci.ro>
2018-10-23 02:03:20 +0300

    added engine

---
+++ CollatzConjecture.Engine/CollatzConjectureEngine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CollatzConjecture.Engine
+{
+    public class CollatzConjectureEngine : ICollatzConjectureEngine
+    {
+        public ulong Compute(ulong number)
+        {
+            return Compute(number, 1);
+        }
+
+        private ulong Compute(ulong number, ulong step)
+        {
+            while (true)
+            {
+                var intermediary = number % 2 == 0 ? number : 3 * number + 1;
+                var next = intermediary / 2;
+
+                Console.WriteLine($"[{step}]: {number} => {intermediary} => {next}");
+
+                if (number == 1)
+                {
+                    return step;
+                }
+
+                number = next;
+                step = ++step;
+            }
+        }
+    }
+}
---
+++ CollatzConjecture.Engine/ICollatzConjectureEngine.cs
@@ -0,0 +1,11 @@
+﻿namespace CollatzConjecture.Engine
+{
+    public interface ICollatzConjectureEngine
+    {
+        /// <summary>
+        /// Implements the Collatz Conjecture
+        /// </summary>
+        /// <param name="number"><see cref="ulong"/> => The number</param>
+        ulong Compute(ulong number);
+    }
+}
